1af5bfaeaf99f66339b841264c46df0eadf501c3
juanka2023 <bull-ter@hotmail.es>
2023-04-20 13:45:44 +0200

    activity nivel1

--- app/src/main/java/com/example/operaciones/MainActivity.java
+++ app/src/main/java/com/example/operaciones/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.operaciones;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    //Método el botón Siguiente
+    public void jugar(View view){
+        Intent jugar = new Intent(this,Mainnivel1.class);
+        startActivity(jugar);
     }
 }
