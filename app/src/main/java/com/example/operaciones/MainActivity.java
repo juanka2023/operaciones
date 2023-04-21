2ba471da21adbe1ff99938aaf91ccff99bf030d5
juanka2023 <bull-ter@hotmail.es>
2023-04-21 11:36:27 +0200

    modificacion nueva

--- app/src/main/java/com/example/operaciones/MainActivity.java
+++ app/src/main/java/com/example/operaciones/MainActivity.java
@@ -20,7 +20,6 @@ public class MainActivity extends AppCompatActivity {
         Intent jugar = new Intent(this,Mainnivel1.class);
         startActivity(jugar);
         finish();
-    } else {
-        Toast.makeText(this, "Primero debes escribir tu nombre", Toast.LENGTH_SHORT).show();
+
     }
 }
