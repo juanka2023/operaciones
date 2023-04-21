91c007944780a9d7429fd17c35fdc75ec62befdb
juanka2023 <bull-ter@hotmail.es>
2023-04-21 09:16:54 +0200

    mas completo

--- app/src/main/java/com/example/operaciones/MainActivity.java
+++ app/src/main/java/com/example/operaciones/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,10 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
     //Método el botón Siguiente
     public void jugar(View view){
         Intent jugar = new Intent(this,Mainnivel1.class);
         startActivity(jugar);
+        finish();
+    } else {
+        Toast.makeText(this, "Primero debes escribir tu nombre", Toast.LENGTH_SHORT).show();
     }
 }
