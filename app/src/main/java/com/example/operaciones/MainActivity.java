f03015e0721c8c1e0ea8a0e5331cfde1ec169b0c
juanka2023 <bull-ter@hotmail.es>
2023-04-24 11:06:27 +0200

    corregido

--- app/src/main/java/com/example/operaciones/MainActivity.java
+++ app/src/main/java/com/example/operaciones/MainActivity.java
@@ -2,12 +2,13 @@ package com.example.operaciones;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.content.Intent;
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.view.View;
 
+
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -19,18 +20,21 @@ public class MainActivity extends AppCompatActivity {
     private ImageView iv_personaje;
     private TextView tv_record;
 
+    int num_aleatorio = (int) (Math.random() * 3);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    }
+
+
+
+        }
     //Método el botón Siguiente
     public void jugar(View view) {
         Intent jugar = new Intent(this, Mainnivel1.class);
         startActivity(jugar);
         finish();
-
-
     }
 }
--- app/src/main/java/com/example/operaciones/Mainnivel1.java
+++ app/src/main/java/com/example/operaciones/Mainnivel1.java
@@ -11,14 +11,6 @@ import android.widget.Toast;
 
 public class Mainnivel1 extends AppCompatActivity {
 
-    private TextView tv_nombre, tv_score;
-    private ImageView ivvuno, ivvdos, avvida;
-    private EditText et_respuesta;
-
-    int score, numAlealeatorio_uno,numAlealeatorio_dos, resultado, vida = 3;
-    String nombre_jugador, string_score, string_vidas;
-
-    String numero [] = {"cero","uno","dos","tres","cuatro","cinco","seis","siete","ocho","nueve"};
 
     @SuppressLint({"MissingInflatedId", "RestrictedApi"})
     @Override
@@ -27,20 +19,7 @@ public class Mainnivel1 extends AppCompatActivity {
         setContentView(R.layout.activity_mainnivel1);
 
 
-        Toast.makeText(this, "nivel 1, sumas basicas", Toast.LENGTH_SHORT).show();
 
-        tv_nombre = (TextView) findViewById(R.id.tvnombre);
-        tv_score = (TextView) findViewById(R.id.tvscore);
-        avvida = (ImageView) findViewById(R.id.iv_vida);
-        ivvdos= (ImageView) findViewById(R.id.ivvalordos);
-        ivvuno = (ImageView) findViewById(R.id.ivvalor1);
-        et_respuesta = (EditText) findViewById(R.id.et_respuestas);
-
-        nombre_jugador = getIntent().getStringExtra("jugador");
-        tv_nombre.setText("Jugador: " + nombre_jugador);
-
-        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setIcon(R.mipmap.ic_launcher);
 
     }
 
